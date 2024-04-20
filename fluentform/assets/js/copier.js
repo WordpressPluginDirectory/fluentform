d2327fe49397fecdf995725017dec3f356219980
Justin Gardner <justin@rhynorater.com>
2024-04-20 06:02:35 -0500

    5.1.14

--- fluentform/assets/js/copier.js
+++ fluentform/assets/js/copier.js
@@ -1,9 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!******************************************!*\
-  !*** ./resources/assets/admin/copier.js ***!
-  \******************************************/
-var clipboard = new ClipboardJS(".btn");
-/******/ })()
-;
-//# sourceMappingURL=copier.js.map
+new ClipboardJS(".btn");
